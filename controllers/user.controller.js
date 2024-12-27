48bdbee3bc32e9edd71304d3eed0e299d23976a0
Abhisheklodha01 <abhisheklodha512@gmail.com>
2024-12-28 01:33:28 +0530

    created comment and post schema and added relationship between them

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -103,3 +103,22 @@ export const getUserByID = async (req, res) => {
         })
    }
 }
+export const deleteUser = async (req, res) => {
+    const id = req.params.id
+  try {
+    const response = await prisma.user.delete({
+        where:{
+            id:Number(id)
+        }
+    })
+    return res.status(200).json({
+     message: "user deleted successfully",
+     response
+    })
+  } catch (error) {
+     return res.status(500).json({
+            message: "Internal server error",
+            error
+        })
+   }
+}
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,5 +1,5 @@
 import {Router} from 'express'
-import { forgotPassword, getUserByID, getUsers, registerUser } from '../controllers/user.controller.js'
+import { deleteUser, forgotPassword, getUserByID, getUsers, registerUser } from '../controllers/user.controller.js'
 
 
 const router = Router()
@@ -8,6 +8,7 @@ router.post("/register", registerUser)
 router.put("/forgot-password", forgotPassword)
 router.get("/getusers", getUsers)
 router.get("/getuser/:id", getUserByID)
+router.delete("/deleteuser/:id", deleteUser)
 
 
 export default router
